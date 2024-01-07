1efee531985945fd6dabdffa51648092f39440e8
skamen13 <maximusgilgenberg@gmail.com>
2024-01-07 19:01:05 +0300

    Added all

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const axios = require("axios");
+
+const app = express();
+const port = 3000;
+
+const endpoint =
+    "https://us-central1-chat-for-chatgpt.cloudfunctions.net/basicUserRequestBeta";
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const notesDir = 'notes/';
+
+if (!fs.existsSync(notesDir)) {
+    fs.mkdirSync(notesDir);
+}
+
+app.get('/', (req, res) => {
+    const notes = fs.readdirSync(notesDir);
+    res.render('index', { notes });
+});
+
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+
+app.get('/edit/:fileName', (req, res) => {
+    const fileName = req.params.fileName;
+    const filePath = `${notesDir}${fileName}`;
+    const content = fs.readFileSync(filePath, 'utf-8');
+
+    res.render('edit', { fileName, content });
+});
+
+app.post('/edit/:fileName', (req, res) => {
+    const fileName = req.params.fileName;
+    const filePath = `${notesDir}${fileName}`;
+    const content = req.body.content;
+
+    fs.writeFileSync(filePath, content);
+
+    res.redirect('/');
+});
+
+// Новый маршрут для генерации текста
+app.post('/generate-text', async (req, res) => {
+    // Фиктивный API для генерации текста
+    const task = req.body.task || 'Default task';
+
+    try {
+        const response = await axios.post(
+            endpoint,
+            {
+                data: {
+                    message:
+                        'Продолжите текст, используя HTML знаки для изменения шрифтов, цветов, и т.д., ваша цель: "' + task.split('$')[0] + '". Вот этот текст: ' + task.split('$')[1],
+                },
+            },
+            {
+                headers: {
+                    Host: 'us-central1-chat-for-chatgpt.cloudfunctions.net',
+                    Connection: 'keep-alive',
+                    Accept: '*/*',
+                    'User-Agent': 'com.tappz.aichat/1.2.2 iPhone/16.3.1 hw/iPhone12_5',
+                    'Accept-Language': 'en',
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+            }
+        );
+
+        const result = response.data.result.choices[0].text;
+        console.log(result);
+        const generatedText = result;
+
+        res.json({ generatedText });
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+
+
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});
